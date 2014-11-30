8521e0dbb7721e6a260a814586543e5e880a5ddd
Alex Scott <alex.scott@tipsforthings.com>
2014-11-30 13:18:07 +0000

    Add titles, helper and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'main/home'
+  root 'main#home'
 
   get 'main/help'
 
--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,39 +1,50 @@
 require 'test_helper'
 
 class MainControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Track My Mixes"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get news" do
     get :news
     assert_response :success
+    assert_select "title", "News | #{@base_title}"
   end
 
   test "should get blog" do
     get :blog
     assert_response :success
+    assert_select "title", "Blog | #{@base_title}"
   end
 
   test "should get terms" do
     get :terms
     assert_response :success
+    assert_select "title", "Terms | #{@base_title}"
   end
 
   test "should get privacy" do
     get :privacy
     assert_response :success
+    assert_select "title", "Privacy | #{@base_title}"
   end
 
 end
